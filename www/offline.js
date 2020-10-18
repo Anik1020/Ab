c28b83bf4789ee49a3440786ee0163f21c14f8c1
Anik1020 <69672268+Anik1020@users.noreply.github.com>
2020-10-18 13:47:06 +0400

    Add files via upload

---
+++ www/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1603013902,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/bugenemy-sheet0.png",
+		"images/crescentenemy-sheet0.png",
+		"images/fighterenemy-sheet0.png",
+		"images/bladeenemy-sheet0.png",
+		"images/saucerenemy-sheet0.png",
+		"images/scytheenemy-sheet0.png",
+		"images/slicerenemy-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
